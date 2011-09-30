02953df2b3f12451620c150f5a9f1c1a16fce834
Sokolov Innokenty <r2.kenny@gmail.com>
2011-10-01 02:17:32 +0400

    email notify for mutt

---
+++ home/sokolov/scrpts/email_notifier.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true; do
+    ev=`inotifywait -e create -e moved_to ~/Mail/*/*/new 2> /dev/null`
+    pa=${ev/ */}
+    f="$pa/${ev/* /}"
+    bo=${pa/\/new/}
+    bo=${bo/*Mail\//}
+    notify-send -t 12000 -i '/usr/share/icons/oxygen/32x32/status/mail-unread-new.png' "New mail in $bo" "`grep -m 1 ^From: $f | sed 's/[<>]//g'`\n`grep -m 1 ^Subject: $f | ~/scrpts/decode_mime_header | sed 's/[<>]//g'`"
+done
