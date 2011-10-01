172cd084998f436f859d5ab8ffcfe87062cd121b
Sokolov Innokenty <r2.kenny@gmail.com>
2011-10-02 02:03:11 +0400

    alternative dzen notify

--- home/sokolov/scrpts/email_notifier.sh
+++ home/sokolov/scrpts/email_notifier.sh
@@ -6,5 +6,6 @@ while true; do
     f="$pa/${ev/* /}"
     bo=${pa/\/new/}
     bo=${bo/*Mail\//}
-    notify-send -t 12000 -i '/usr/share/icons/oxygen/32x32/status/mail-unread-new.png' "New mail in $bo" "`grep -m 1 ^From: $f | sed 's/[<>]//g'`\n`grep -m 1 ^Subject: $f | ~/scrpts/decode_mime_header | sed 's/[<>]//g'`"
+    # ~/sft/dwm/dzen/notify "New mail in $bo\n`grep -m 1 ^From: $f`\n`grep -m 1 ^Subject: $f | ~/scrpts/decode_mime_header`"
+    notify-send -t 12000 -i '/usr/share/icons/oxygen/32x32/status/mail-unread-new.png' "New mail in $bo" "`grep -m 1 ^From: $ | sed 's/[<>]//g'`\n`grep -m 1 ^Subject: $f | ~/scrpts/decode_mime_header | sed 's/[<>]//g'`"
 done
