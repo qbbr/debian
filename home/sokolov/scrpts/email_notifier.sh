49f92e4a3e9e15cd0015f02e60949028fbf04815
Sokolov Innokenty <r2.kenny@gmail.com>
2011-10-02 19:39:25 +0400

    fix From, n full patch

--- home/sokolov/scrpts/email_notifier.sh
+++ home/sokolov/scrpts/email_notifier.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 while true; do
-    ev=`inotifywait -e create -e moved_to ~/Mail/*/*/new 2> /dev/null`
+    ev=`inotifywait -e create -e moved_to /home/qbbr/Mail/*/*/new 2> /dev/null`
     pa=${ev/ */}
     f="$pa/${ev/* /}"
     bo=${pa/\/new/}
     bo=${bo/*Mail\//}
     # ~/sft/dwm/dzen/notify "New mail in $bo\n`grep -m 1 ^From: $f`\n`grep -m 1 ^Subject: $f | ~/scrpts/decode_mime_header`"
-    notify-send -t 12000 -i '/usr/share/icons/oxygen/32x32/status/mail-unread-new.png' "New mail in $bo" "`grep -m 1 ^From: $ | sed 's/[<>]//g'`\n`grep -m 1 ^Subject: $f | ~/scrpts/decode_mime_header | sed 's/[<>]//g'`"
+    notify-send -t 12000 -i '/usr/share/icons/oxygen/32x32/status/mail-unread-new.png' "New mail in $bo" "`grep -m 1 ^From: $f | sed 's/[<>]//g'`\n`grep -m 1 ^Subject: $f | ~/scrpts/decode_mime_header | sed 's/[<>]//g'`"
 done
